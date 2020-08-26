e8b7074502defc3010ed17ccf3790ed5aae7c656
Razaul Munir <razaulmunir@gmail.com>
2020-08-27 00:10:08 +0600

    AffectedCountries activity added

---
+++ app/src/main/java/org/myself/trackerapp/Activity/AffectedCountriesActivity.java
@@ -0,0 +1,16 @@
+package org.myself.trackerapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.myself.trackerapp.R;
+
+public class AffectedCountriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country);
+    }
+}
--- app/src/main/java/org/myself/trackerapp/Activity/MainActivity.java
+++ app/src/main/java/org/myself/trackerapp/Activity/MainActivity.java
@@ -2,6 +2,7 @@ package org.myself.trackerapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -116,5 +117,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goTrackCountries(View view) {
+        startActivity(new Intent(getApplicationContext(), AffectedCountriesActivity.class));
     }
 }
