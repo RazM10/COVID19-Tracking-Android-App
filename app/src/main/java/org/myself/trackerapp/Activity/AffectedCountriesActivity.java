e22e8219ca717cde7c401327a8a00ecffdfe24bb
Razaul Munir <razaulmunir@gmail.com>
2020-08-28 16:11:21 +0600

    country list adapter added

--- app/src/main/java/org/myself/trackerapp/Activity/AffectedCountriesActivity.java
+++ app/src/main/java/org/myself/trackerapp/Activity/AffectedCountriesActivity.java
@@ -3,10 +3,27 @@ package org.myself.trackerapp.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.myself.trackerapp.Adapter.CountryAdapter;
+import org.myself.trackerapp.Model.Country;
 import org.myself.trackerapp.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AffectedCountriesActivity extends AppCompatActivity {
+
+    EditText edtSearch;
+    ListView listView;
+    SimpleArcLoader simpleArcLoader;
+
+    public static List<Country> countryModelsList = new ArrayList<>();
+    Country countryModel;
+    CountryAdapter myCustomAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/org/myself/trackerapp/Adapter/CountryAdapter.java
@@ -0,0 +1,109 @@
+package org.myself.trackerapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+
+import org.myself.trackerapp.Activity.AffectedCountriesActivity;
+import org.myself.trackerapp.Model.Country;
+import org.myself.trackerapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryAdapter extends ArrayAdapter<Country> {
+
+    private Context context;
+    private List<Country> countryList;
+    private List<Country> countryListFiltered;
+
+    public CountryAdapter(Context context, List<Country> countryModelsList) {
+        super(context, R.layout.item_country, countryModelsList);
+
+        this.context = context;
+        this.countryList = countryModelsList;
+        this.countryListFiltered = countryModelsList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_country, null, true);
+        TextView tvCountryName = view.findViewById(R.id.tvCountryName);
+        ImageView imageView = view.findViewById(R.id.imageFlag);
+
+        tvCountryName.setText(countryListFiltered.get(position).getCountry());
+        Glide.with(context).load(countryListFiltered.get(position).getFlag()).into(imageView);
+
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        return countryListFiltered.size();
+    }
+
+    @Nullable
+    @Override
+    public Country getItem(int position) {
+        return countryListFiltered.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+
+                FilterResults filterResults = new FilterResults();
+                if(constraint == null || constraint.length() == 0){
+                    filterResults.count = countryList.size();
+                    filterResults.values = countryList;
+
+                }else{
+                    List<Country> resultsModel = new ArrayList<>();
+                    String searchStr = constraint.toString().toLowerCase();
+
+                    for(Country itemsModel: countryList){
+                        if(itemsModel.getCountry().toLowerCase().contains(searchStr)){
+                            resultsModel.add(itemsModel);
+
+                        }
+                        filterResults.count = resultsModel.size();
+                        filterResults.values = resultsModel;
+                    }
+
+
+                }
+
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+
+                countryListFiltered = (List<Country>) results.values;
+                AffectedCountriesActivity.countryModelsList = (List<Country>) results.values;
+                notifyDataSetChanged();
+
+            }
+        };
+        return filter;
+    }
+}
