7b98b8fad46ec1e6537890e3d9d12bf84966876b
Razaul Munir <razaulmunir@gmail.com>
2020-08-28 16:31:40 +0600

    get country resultes code added

--- app/src/main/java/org/myself/trackerapp/Helper/Links.java
+++ app/src/main/java/org/myself/trackerapp/Helper/Links.java
@@ -2,4 +2,5 @@ package org.myself.trackerapp.Helper;
 
 public class Links {
     public static String allLink = "https://corona.lmao.ninja/v2/all/";
+    public static String countryLink = "https://corona.lmao.ninja/v2/countries/";
 }
